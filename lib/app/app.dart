d2bfb34f86c0990688dd9781a848a7fff3e25cde
GabrielVidigal <81254054+GabrielVidigal@users.noreply.github.com>
2022-06-14 21:37:20 -0300

    git add .

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -42,6 +42,7 @@ class App extends StatelessWidget {
           "/home": (context) => const HomePage(),
           "/candidacy": (context) => const CandidacyContainer(),
           "/profile": (context) => const ProfileContainer(),
+          "/register2": (context) => const RegisterPage(),
         },
       ),
     );
