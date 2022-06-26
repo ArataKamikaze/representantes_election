d6437e231ced12d10cfde5c6ba0fb352a966a3ce
Diogo Dias Fontoura <diogodias1705@gmail.com>
2022-06-26 11:28:30 -0300

    mudanças firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,22 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart';
 import 'app/app.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Window.initialize();
   runApp(const App());
-  doWhenWindowReady(
-    () {
-      appWindow
+  doWhenWindowReady(() {
+    appWindow
       ..minSize = const Size(640, 360)
       ..size = const Size(720, 540)
       ..alignment = Alignment.center
-      ..maximize()  
+      ..maximize()
       ..show();
-    }
-  );
+  });
   await Window.setEffect(
     effect: WindowEffect.acrylic,
-    color:const Color.fromARGB(50, 13, 38, 45),
+    color: const Color.fromARGB(50, 13, 38, 45),
   );
 }
